524e3e9a852beaa66d2ddb6a746700307fd6bdea
Paul Griz <paulericgriz@gmail.com>
2023-01-16 14:17:23 -0500

    Added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="bg-gray-900 p-4 shadow md:flex md:items-center md:justify-between md:p-6">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2023{" "}
+        <a href="https://flowbite.com/" class="hover:underline">
+          Paul Griz
+        </a>
+        . All Rights Reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
